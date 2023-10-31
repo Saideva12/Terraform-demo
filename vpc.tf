39b3662155f959b25c9c67a65a4877b93dfdb713
Saideva12 <saidevabhakthuni@gmail.com>
2023-10-31 12:32:23 +0000

    create Pub subnet A&B

--- vpc.tf
+++ vpc.tf
@@ -26,3 +26,23 @@ resource "aws_subnet" "tf-ecomm-pub-sn-B" {
     Name = "ecomm-pub-subnet-B"
   }
 }
+
+resource "aws_subnet" "tf-ecomm-pvt-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.0.2.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-A"
+  }
+}
+
+resource "aws_subnet" "tf-ecomm-pvt-sn-B" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.0.3.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-B"
+  }
+}
