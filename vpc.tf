c90f58a0179f93af6e5b9688f8ff3b6f0c5c5d1a
Saideva12 <saidevabhakthuni@gmail.com>
2023-10-31 12:10:19 +0000

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "tf-ecomm" {
     Name = "vpc-ecomm"
   }
 }
+
+resource "aws_subnet" "tf-ecomm-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.0.0.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "ecomm-pub-subnet-A"
+  }
+}
